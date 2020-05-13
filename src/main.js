bcac2d23a0ca167a2e348bc2e5324778fd51f2b4
BlackCrowxyz <p.mohammadkhani95@gmail.com>
2020-05-13 22:24:38 +0430

    Vuelidate Module added

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import Vuelidate from 'vuelidate'
 Vue.config.productionTip = false;
 
-
+Vue.use(Vuelidate)
 
 new Vue({
   router,
